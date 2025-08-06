d0ad4b1d1660e615385db6df6a7df8d15cc4fa71
Lucien Dulac <coulterfeith@live.fr>
2025-08-06 13:49:47 +0200

    chore: fix eslint config

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+const path = require('path');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+module.exports = [
+  js.configs.recommended,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        React: 'readonly',
+        JSX: 'readonly',
+      },
+    },
+  },
+]; 
