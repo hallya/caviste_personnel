0397e1f9c998280fa2aa0777cdb1ed20e033d336
Lucien Dulac <coulterfeith@live.fr>
2025-08-07 20:58:21 +0200

    feat: update meta with image and social media

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,33 @@ import LayoutContainer from "./components/layout/containers/LayoutContainer";
 export const metadata: Metadata = {
   title: "Edouard, Caviste personnel",
   description:
-    "Caviste personnel à Paris : vins de vignerons français, sélection confidentielle, conseils personnalisés, livraison sur mesure et tarifs négociés.",
+    "Vins de vignerons français, sélection confidentielle, conseils personnalisés, livraison sur mesure et tarifs négociés.",
   icons: {
     icon: "/favicon.svg",
     shortcut: "/favicon.svg",
     apple: "/favicon.svg",
+  },
+  openGraph: {
+    title: "Edouard, Caviste personnel",
+    description: "Vins de vignerons français, sélection confidentielle, conseils personnalisés, livraison sur mesure et tarifs négociés.",
+    url: "https://caviste-personnel.vercel.app",
+    siteName: "Edouard, Caviste personnel",
+    images: [
+      {
+        url: "/edouard.png",
+        width: 1200,
+        height: 630,
+        alt: "Un homme souriant tenant un verre de vin blanc dans une cave à vin, illustration stylisée.",
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Edouard, Caviste personnel",
+    description: "Vins de vignerons français, sélection confidentielle, conseils personnalisés, livraison sur mesure et tarifs négociés.",
+    images: ["/og-image.svg"],
   },
 };
 
